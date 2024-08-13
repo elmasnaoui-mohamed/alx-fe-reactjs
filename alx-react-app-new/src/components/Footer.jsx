26953d242ac1de53ab16feddb96d054b9c8a6bc7
Mohamed EL MASNAOUI <mohamed.elmasnaoui@intalio.com>
2024-08-13 15:05:49 +0100

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: '#009688'}}>
+            <p  style={{ color: 'white', textAlign: 'center' }}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: '#eee'}}>
+            <p style={{ color: 'black', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
