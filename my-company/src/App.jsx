bae866edac61c25769e102a11fd9b01e97fdd939
Mohamed EL MASNAOUI <mohamed.elmasnaoui@intalio.com>
2024-08-15 01:00:48 +0100

    Build a Simple Company Website with React

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,7 +4,7 @@ import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
