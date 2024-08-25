6e490ab9a199fd89e21a6c8127923fc0d3775ee4
Mohamed EL MASNAOUI <mohamed.elmasnaoui@intalio.com>
2024-08-25 17:10:01 +0100

    Implementing User Favorites and Personalized Recipe Recommendations

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
@@ -13,6 +15,8 @@ function App() {
         <SearchBar />
         <Routes>
           <Route path="/" element={<RecipeList />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
           <Route path="/add" element={<AddRecipeForm />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
