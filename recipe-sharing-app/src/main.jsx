4dd56323cd68bab06879e0b996a8250413bcfe06
Mohamed EL MASNAOUI <mohamed.elmasnaoui@intalio.com>
2024-09-07 03:15:13 +0100

    Initialize the Recipe Sharing Application with Zustand State Management

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
-import RecipeDetails from './components/RecipeDetails';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/recipe/:id" element={<RecipeDetails />} />
-    </Routes>
-  </Router>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
