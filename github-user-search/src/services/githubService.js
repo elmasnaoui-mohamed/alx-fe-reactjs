d27f2c20c60eb63ca4c1a4d25a82461767622ac4
Mohamed EL MASNAOUI <mohamed.elmasnaoui@intalio.com>
2024-09-20 18:26:26 +0100

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_API_URL = 'https://api.github.com/search/users?q=';
 
-export const fetchUserData = (username) => {
-  return axios.get(`${GITHUB_API_URL}/${username}`);
+export const fetchAdvancedUserData = (query) => {
+  return axios.get(`${GITHUB_API_URL}${query}`);
 };
