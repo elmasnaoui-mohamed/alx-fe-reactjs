7be5a7efbd1b8c878990b4dc0014f2a1d7bcc4c7
Mohamed EL MASNAOUI <mohamed.elmasnaoui@intalio.com>
2024-09-22 16:12:01 +0100

    1. Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,21 +1,12 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com/search/users?q=';
+const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_SEARCH_API_URL = 'https://api.github.com/search/users?q=';
 
-export const fetchAdvancedUserData = (username, location, minRepos) => {
-  let query = '';
+export const fetchUserData = (username) => {
+  return axios.get(`${GITHUB_API_URL}/${username}`);
+};
 
-  if (username) {
-    query += `${username}`;
-  }
-
-  if (location) {
-    query += `+location:${location}`;
-  }
-
-  if (minRepos) {
-    query += `+repos:>${minRepos}`;
-  }
-
-  return axios.get(`${GITHUB_API_URL}${query}`);
+export const fetchAdvancedUserData = (query) => {
+  return axios.get(`${GITHUB_SEARCH_API_URL}${query}`);
 };
